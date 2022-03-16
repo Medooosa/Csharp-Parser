8808a0cc92ad10570ab6d017e4173e0e95fc54eb
schu1609 <schu1609@student.nhl.nl>
2022-03-16 22:04:59 +0100

    BiographiesActorList toegevoegd met code hoe die door de file heen kijkt.

---
+++ Csharp Parser/ConsoleApp1/BiographiesActorList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    public class BiographiesActorList
+    {
+        string temp = string.Empty;
+        string BiographiesActorFileName = @"E:\Big movie files\biographies.list";
+        string editedActorBiographies = @"..\..\..\testfiles\editedactorbiographies.csv";
+        public BiographiesActorList(string fileLocation = @"E:\Big movie files\Csharp-Parser\Csharp Parser\ConsoleApp1\Biographiesactortestfile.txt")
+        {
+            BiographiesActorFileName = fileLocation;
+        }
+        public void RunParser(string path = @"..\..\..\testfiles\editedactorbiographies.csv")
+        {
+            string line;
+            StreamReader sr = new StreamReader(BiographiesActorFileName, System.Text.Encoding.GetEncoding(28591));
+            StreamWriter sw = new StreamWriter(editedActorBiographies);
+            string[] nameAndBirth = new string[2];
+            while ((line = sr.ReadLine()) != null)
+            {
+                int temp = line.Length;
+                if (line.StartsWith("-"))
+                {
+                    if (nameAndBirth[1] != null)
+                    {
+                        sw.WriteLine(nameAndBirth[0] + "¤" + nameAndBirth[1]);    
+                    }
+                    nameAndBirth = new string[2];
+                }
+                if (line.StartsWith("NM"))
+                    nameAndBirth[0] = line.Substring(4, line.Length - 4);
+                if (line.StartsWith("DB"))
+                    nameAndBirth[1] = line.Substring(4, line.Length - 4);
+            }
+            sw.WriteLine(nameAndBirth[0] + "¤" + nameAndBirth[1]);
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- Csharp Parser/ConsoleApp1/Program.cs
+++ Csharp Parser/ConsoleApp1/Program.cs
@@ -7,43 +7,50 @@ namespace ConsoleApp1
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            string pattern = @"^(?![\""])(.*).(\([0-9,?,/,A-Z]{4,11}\)).((\([a-z,A-Z,?]{1,}\))|(.*)).*([0-9,?]{4})";
-            string substitution = "$1¤$2¤$4¤$5¤$6";
-            string movieTestFile = @"E:\Big movie files\Csharp-Parser\Csharp Parser\ConsoleApp1\Moviestestfile.txt";
-            string movieFile = @"E:\Big movie files\movies.list";
-            StreamReader sr = new StreamReader(movieFile,System.Text.Encoding.GetEncoding(28591));
-            StreamWriter sw = new StreamWriter(@"..\..\..\testfiles\editedmovies2.csv");
-            sw.WriteLine("TITLE¤STARTYEAR¤ATTRIBUTE¤NOTE¤ENDYEAR");
-            RegexOptions options = RegexOptions.Singleline;
-            Regex regexPattern = new Regex(pattern, options);
-            string result = string.Empty;
-            string line;
-            while ((line = sr.ReadLine()) != null )
-            {
-                Match m = Regex.Match(line, pattern, options);
-                if (m.Success)
-                {
-                    result = m.Value;
-                    result = regexPattern.Replace(result, substitution);
-                    result = Regex.Replace(result, @"\t+", ""); //om files te editen.
-                    sw.WriteLine(result);
-                }               
-            }
-            sr.Close();
-            sw.Close();
-            //foreach (Match m in Regex.Matches(lineInput, pattern, options))
+            //string patternMovies = @"^(?![\""])(.*).(\([0-9,?,/,A-Z]{4,11}\)).((\([a-z,A-Z,?]{1,}\))|(.*)).*([0-9,?]{4})";
+            //string patternSeries = @"(\"".*\"").*(\([0-9,?,/,A-Z]{4,11}\)).((({.*}) ({.*})|({.*}))|(\t))\t+([0-9,?,-]{4,9})";
+            //string substitutionMovies = "$1¤$2¤$4¤$5¤$6";
+            //string substitutionSeries = "$1¤$2¤$5$7¤$6¤$9";
+            //string movieTestFile = @"E:\Big movie files\Csharp-Parser\Csharp Parser\ConsoleApp1\Moviestestfile.txt";
+            //string movieActorFile = @"E:\Big movie files\movies.list";
+            //string startLineMovies = "TITLE¤STARTYEAR¤ATTRIBUTE¤NOTE¤ENDYEAR";
+            //string startLineSeries = "TITLE¤STARTYEAR¤TITLESERIES¤NOTE¤RUNTIMEYEAR/ENDYEAR";
+            //string editedMoviesCsv = @"..\..\..\testfiles\editedmovies.csv";
+            //string editedSeriesCsv = @"..\..\..\testfiles\editedseries.csv";
+            //StreamReader sr = new StreamReader(movieActorFile,System.Text.Encoding.GetEncoding(28591));
+            //StreamWriter sw = new StreamWriter(editedSeriesCsv);
+
+            //sw.WriteLine(startLineSeries);
+
+            //RegexOptions options = RegexOptions.Singleline;
+            //Regex regexPattern = new Regex(patternSeries, options);
+
+            //string result = string.Empty, line;
+            //while ((line = sr.ReadLine()) != null )
             //{
-            //    result = m.Value + '\n';
-            //    result = regexPattern.Replace(result, substitution);
-            //    result = Regex.Replace(result, @"\t+", "");
-            //    File.AppendAllText(@"..\..\..\testfiles\editedmovies.csv", result);
+            //    Match m = Regex.Match(line, patternSeries, options);
+            //    if (m.Success)
+            //    {
+            //        result = m.Value;
+            //        result = regexPattern.Replace(result, substitutionSeries);
+            //        result = Regex.Replace(result, @"\t+", ""); //om files tabs te removen
+            //        sw.WriteLine(result);
+            //    }               
             //}
-            //result = regexPattern.Replace(result, substitution);
-            //result = Regex.Replace(result, @"\t+", ""); //om files te editen.
+            //sr.Close();
+            //sw.Close();
+            BiographiesActorList BAL = new BiographiesActorList(@"E:\Big movie files\biographies.list");
+            BAL.RunParser();
             //Console.WriteLine(Environment.CurrentDirectory);
         }
     }
 }
+//foreach (Match m in Regex.Matches(lineInput, pattern, options))
+//{
+//    result = m.Value + '\n';
+//    result = regexPattern.Replace(result, substitution);
+//    result = Regex.Replace(result, @"\t+", "");
+//    File.AppendAllText(@"..\..\..\testfiles\editedmovies.csv", result);
+//}
