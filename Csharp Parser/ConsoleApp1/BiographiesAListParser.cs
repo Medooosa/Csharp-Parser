b6178b643798fe39273b202c9f6cb20c766fb11a
schu1609 <schu1609@student.nhl.nl>
2022-03-22 16:42:27 +0100

    BiographiesAListParser aangepast

--- Csharp Parser/ConsoleApp1/BiographiesAListParser.cs
+++ Csharp Parser/ConsoleApp1/BiographiesAListParser.cs
@@ -13,27 +13,41 @@ namespace ConsoleApp1{
         }
 
         public override void RunParser(){
-            try{
+            try
+            {
                 string line;
                 StreamReader sr = new StreamReader(fileLocation, System.Text.Encoding.GetEncoding(28591));
                 StreamWriter sw = new StreamWriter(fileName);
-                string[] nameAndBirth = new string[2];
+                string[] nameAndBirth = new string[3];
                 while ((line = sr.ReadLine()) != null)
                 {
                     if (line.StartsWith("-"))
                     {
                         if (nameAndBirth[1] != null)
                         {
-                            sw.WriteLine(nameAndBirth[0] + "¤" + nameAndBirth[1]);    
+                            sw.WriteLine(nameAndBirth[0] + "¤" + nameAndBirth[1] + "¤" + nameAndBirth[2]);    
                         }
-                        nameAndBirth = new string[2];
+                        nameAndBirth = new string[3];
                     }
                     if (line.StartsWith("NM"))
                         nameAndBirth[0] = line.Substring(4, line.Length - 4);
                     if (line.StartsWith("DB"))
-                        nameAndBirth[1] = line.Substring(4, line.Length - 4);
+                    {
+                        string result = line.Substring(4, line.Length - 4);
+                        string[] splitted = result.Split(',');
+                        for (int i = 0; i < splitted.Length; i++)
+                        {
+                            if (i == 0)
+                                nameAndBirth[1] = splitted[i];
+                            else if (i == 1)
+                                nameAndBirth[2] = nameAndBirth[2] + splitted[i];
+                            else
+                                nameAndBirth[2] = nameAndBirth[2] + "," + splitted[i];
+                        }
+                        //nameAndBirth[1] = line.Substring(4, line.Length - 4);
+                    }
                 }
-                sw.WriteLine(nameAndBirth[0] + "¤" + nameAndBirth[1]);
+                sw.WriteLine(nameAndBirth[0] + "¤" + nameAndBirth[1] + "¤" + nameAndBirth[2]);
                 sr.Close();
                 sw.Close();
             }catch (Exception e){
