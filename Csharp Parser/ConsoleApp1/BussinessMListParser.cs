8e78ce321b6996dfcf27c1de6a37c8b458d2d685
sidneydijkstra <sidney0099@hotmail.com>
2022-03-22 17:24:42 +0100

    Changed RunParser function in bussiness class

--- Csharp Parser/ConsoleApp1/BussinessMListParser.cs
+++ Csharp Parser/ConsoleApp1/BussinessMListParser.cs
@@ -8,33 +8,48 @@ namespace ConsoleApp1 {
 
     public class BussinessMListParser : ParserBase {
         
-        public BussinessMListParser(string fileLocation = @"E:\Big movie files\business.list", string newFileName = @"..\..\..\testfiles\business.csv") : base(fileLocation, newFileName){
+        public BussinessMListParser(string fileLocation = @"C:\projects\DataScience\Csharp-Parser\Csharp Parser\ConsoleApp1\business.list", string newFileName = "business.csv") : base(fileLocation, newFileName){
             this.fileMap = @"..\..\..\testfiles\";
         }
 
-        public override void RunParser(){
-            try{
-                string line;
-                StreamReader sr = new StreamReader(this.fileLocation, System.Text.Encoding.GetEncoding(28591));
-                StreamWriter sw = new StreamWriter(this.fileName);
-                sw.WriteLine("Name¤Budget");
-                string[] nameAndBudget = new string[2];
-                while ((line = sr.ReadLine()) != null){
-                    int temp = line.Length;
-                    if (line.StartsWith("MV:")){
-                        nameAndBudget[0] = line.Replace("MV: ", "");
-                    }else if(line.StartsWith("BT:")){
-                        nameAndBudget[1] = line.Replace("BT: ", "");
-                    }else if(line.StartsWith("-") && nameAndBudget[0] != null && nameAndBudget[1] != null){
-                        sw.WriteLine(string.Format("{0}¤{1}", nameAndBudget));
-                        nameAndBudget = new string[2];
-                    }
+public override void RunParser(){
+    try{
+        string line;
+        StreamReader sr = new StreamReader(this.fileLocation, System.Text.Encoding.GetEncoding(28591));
+        StreamWriter sw = new StreamWriter(this.fileName);
+        sw.WriteLine("Name¤Budget");
+        string[] nameYearAndBudget = new string[3];
+        while ((line = sr.ReadLine()) != null){
+            int temp = line.Length;
+            if (line.StartsWith("MV:")){
+                nameYearAndBudget[0] = line.Replace("MV: ", "");
+                string[] split = nameYearAndBudget[0].Split('(');
+                nameYearAndBudget[0] = split.Length > 0 ? split[0] : "";
+                nameYearAndBudget[1] = split.Length > 1 ? split[1].Split(')')[0] : "";
+
+                if(nameYearAndBudget[0].Length > 0){
+                    nameYearAndBudget[0] = nameYearAndBudget[0].Remove(nameYearAndBudget[0].Length-1);
                 }
-                sr.Close();
-                sw.Close();
-            }catch (Exception e){
-                Console.WriteLine(e.ToString());
+                
+            }else if(line.StartsWith("BT:")){
+                nameYearAndBudget[2] = line.Replace("BT: ", "");
+            }else if(line.StartsWith("-") && nameYearAndBudget[0] != null && nameYearAndBudget[2] != null){
+                sw.WriteLine(string.Format("{0}¤{1}¤{2}", nameYearAndBudget));
+                nameYearAndBudget = new string[3];
             }
+        }
+        sr.Close();
+        sw.Close();
+    }catch (Exception e){
+        Console.WriteLine(e.ToString());
+    }
+}
+
+        public string Between(string str, string firstString, string lastString){
+            if(str.Length <= 0) return "XXXX";
+            int Pos1 = str.IndexOf(firstString) + firstString.Length;
+            int Pos2 = str.IndexOf(lastString);
+            return str.Substring(Pos1, Pos2 - Pos1);
         }
 
     }
